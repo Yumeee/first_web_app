70e9b3f4e4214ca487af18b550b85c8805418cf6
YUMEE <yumeehk@hotmail.com>
2018-06-07 22:46:37 -0400

    gallery, favourites added

--- server.rb
+++ server.rb
@@ -9,5 +9,20 @@ get '/portfolio' do
 end
 
 get '/about_me' do
+  @skills = ['git', 'html', 'css', 'ruby']
+  @interests = ['wine', 'tv shows', 'bears']
   erb(:aboutme)
+end
+
+get '/favourites' do
+  @fav_links = ['https://www.netflix.com/browse', 'https://www.facebook.com/', 'https://soundcloud.com/']
+  erb(:fav_links)
+end
+
+get '/' do
+  redirect to ('/home')
+end
+
+get '/gallery' do
+  redirect to ('/portfolio')
 end
